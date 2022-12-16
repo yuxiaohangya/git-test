9c154d9bf8f6422d7e3c95d3ead73705963ad7f6
yuxiaohang <1853589728@qq.com>
2022-12-16 23:10:09 +0800

    first commit

---
+++ src/main/java/com/yxh/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yxh.git;
+
+/**
+ * @author yuxiaohang
+ * @create 2022-12-16 22:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+    }
+}
