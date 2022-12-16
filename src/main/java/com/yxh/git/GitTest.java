2e5a67c100546a5beb5cd55ac6d8740e70f13de7
yuxiaohang <1853589728@qq.com>
2022-12-16 23:16:55 +0800

    second commit

--- src/main/java/com/yxh/git/GitTest.java
+++ src/main/java/com/yxh/git/GitTest.java
@@ -6,6 +6,7 @@ package com.yxh.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git ");
+        System.out.println("hello git");
+        System.out.println("hello git1");
     }
 }
